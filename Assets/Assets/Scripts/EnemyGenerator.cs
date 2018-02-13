932e1a85949b391c05baa4ab3a4301805cd9dd22
Hexagear <tetragears@gmail.com>
2018-02-14 01:00:14 +0300

    небольшие правки

--- Assets/Assets/Scripts/EnemyGenerator.cs
+++ Assets/Assets/Scripts/EnemyGenerator.cs
@@ -3,29 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyGenerator : MonoBehaviour
-{    
+{
     private float spawnDistance;
     private ObjectPooler theObjectPool;
 
     public float platformWidth;
-    public Transform lastSpawn;    
+    public Transform lastSpawn;
     public float spawnChance;
     public Transform lastPlatform;
-    
+
 
     // Use this for initialization
     void Start()
     {
-        theObjectPool = GetComponent<ObjectPooler>();        
+        theObjectPool = GetComponent<ObjectPooler>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (lastSpawn.position.x < lastPlatform.position.x - platformWidth)
+        if (lastSpawn.position.x < lastPlatform.position.x - platformWidth & spawnChance > Random.Range(0, spawnChance))
         {
-            spawnDistance = Random.Range(0, spawnChance);            
-
             GameObject enemy = theObjectPool.GetPooledObject();
             enemy.transform.position = lastPlatform.position;
             enemy.transform.rotation = lastPlatform.rotation;
