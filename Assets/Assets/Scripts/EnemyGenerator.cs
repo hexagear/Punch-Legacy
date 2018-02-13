58e684d6b55f120379f509615057887e4cb88ff6
Hexagear <tetragears@gmail.com>
2018-02-14 01:15:48 +0300

    спавн работает, прототип готов

--- Assets/Assets/Scripts/EnemyGenerator.cs
+++ Assets/Assets/Scripts/EnemyGenerator.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyGenerator : MonoBehaviour
-{
-    private float spawnDistance;
+{    
     private ObjectPooler theObjectPool;
 
+    public float spawnDistance;
+    public int health;
     public float platformWidth;
     public Transform lastSpawn;
-    public float spawnChance;
+    //public float spawnChance;
     public Transform lastPlatform;
 
 
@@ -22,12 +23,15 @@ public class EnemyGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (lastSpawn.position.x < lastPlatform.position.x - platformWidth & spawnChance > Random.Range(0, spawnChance))
+        if (lastSpawn.position.x < lastPlatform.position.x - platformWidth * spawnDistance)
         {
             GameObject enemy = theObjectPool.GetPooledObject();
             enemy.transform.position = lastPlatform.position;
             enemy.transform.rotation = lastPlatform.rotation;
+            EnemyController enemyController = enemy.GetComponent<EnemyController>();
+            enemyController.health = health;
             enemy.SetActive(true);
+            enemy.layer = 10;
             lastSpawn.position = lastPlatform.position;
         }
     }
